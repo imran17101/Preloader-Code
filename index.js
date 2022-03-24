4b79b528e03a21f67ffa04566a1ea11c6a208652
Imran <85269752+imran17101@users.noreply.github.com>
2022-03-24 16:00:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+$(window).on('load', function () { // makes sure that whole site is loaded
+            $('#status').fadeOut();
+            $('#preloader').delay(1000).fadeOut('slow');
+        });
